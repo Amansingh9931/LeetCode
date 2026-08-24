b47acbe0e307ee067f0e3e8448da7906b5560d01
Aman Kumar <147585271+Amansingh9931@users.noreply.github.com>
2026-08-24 15:19:29 +0530

    Time: 9 ms (28.78%), Space: 47.1 MB (43.17%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,18 +1,25 @@
 class Solution {
-  public boolean sumGame(String num) {
-    final int n = num.length();
-    double ans = 0.0;
+    public boolean sumGame(String num) {
+        int n=num.length();
 
-    for (int i = 0; i < n / 2; ++i)
-      ans += getExpectation(num.charAt(i));
+        int leftS=0,rightS=0;
+        int leftQ=0, rightQ=0;
 
-    for (int i = n / 2; i < n; ++i)
-      ans -= getExpectation(num.charAt(i));
-
-    return ans != 0.0;
-  }
-
-  private double getExpectation(char c) {
-    return c == '?' ? 4.5 : c - '0';
-  }
+        for(int i=0;i<n;i++){
+            if(num.charAt(i)=='?'){
+                if(i<n/2)leftQ++;
+                else rightQ++;
+            }else{
+                if(i<n/2)leftS+=num.charAt(i)-'0';
+                else rightS+=num.charAt(i)-'0';
+            }
+        }
+        int totalQ=leftQ+rightQ;
+        if(totalQ%2==1)return true;
+        
+        int left=2*leftS + 9*leftQ;
+        int right=2*rightS + 9*rightQ;
+        if(left==right)return false;
+        else return true;
+    }
 }
